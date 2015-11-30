CyanogenMod/android_device_qcom_common

msm8994: Set a property to track battery mode
This allows us to trigger services based on battery mode.

CYNGNOS-1211

Change-Id: I18097a8ff859d2334f798a9e5cb83a1024057c10

power/power-8994.c
@@ -63,6 +63,12 @@ static void set_power_profile(int profile) {
     if (profile == current_power_profile)
         return;
 
+    char buf[256];
+    sprintf(buf, "%i", profile);
+
+    property_set("sys.thermal.mode", buf);
+
+
     ALOGV("%s: profile=%d", __func__, profile);
 
     if (current_power_profile != PROFILE_BALANCED) {

SlimRoms/device_qcom_common

power/power-8994.c
@@ -63,6 +63,11 @@ static void set_power_profile(int profile) {
     if (profile == current_power_profile)
         return;
 
+    char buf[256];
+    sprintf(buf, "%i", profile);
+
+    property_set("sys.power.mode", buf);
+
     ALOGV("%s: profile=%d", __func__, profile);
 
     if (current_power_profile != PROFILE_BALANCED) {